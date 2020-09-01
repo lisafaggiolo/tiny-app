09c8bd383960f5b211e1da385b829b51e4c9354f
lisafaggiolo <lisa.faggiolo@live.ca>
2020-09-01 17:15:34 +0000

    urlDatabase access bug fixed

--- express_server.js
+++ express_server.js
@@ -9,10 +9,25 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
+
+app.get('/urls/:shortUrl', (req, res) => {
+  let templateVars = { shortURL: req.params.shortUrl, longURL: urlDatabase[req.params.shortUrl] };
+  //console.log(/urls/:shortUrl route);
+  console.log(urlDatabase)
+  console.log(templateVars)
+  res.render('urls_show', templateVars);
+});
+
+/*app.get('/urls/:b2xVn2', (req, res) => {
+  let templateVars = { shortURL: req.params['b2xVn2'], longURL: 'http://www.lighthouselabs.ca' };
+  res.render('urls_show', templateVars);
+});*/
 
 app.get('/', (req, res) => {
   res.send('Hello!');
